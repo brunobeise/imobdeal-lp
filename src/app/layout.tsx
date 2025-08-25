1423d7b13a22d64978ca9707d76fd9aa6118e99c
brunobeise <bruno_beise@hotmail.com>
2025-08-25 14:26:44 -0300

    feat: Atualizar ID do Facebook para rastreamento de eventos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -99,7 +99,7 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1592851651428681');
+              fbq('init', '1253769810303729');
               fbq('track', 'PageView');
             `,
           }}
@@ -114,7 +114,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1592851651428681&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1253769810303729&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
